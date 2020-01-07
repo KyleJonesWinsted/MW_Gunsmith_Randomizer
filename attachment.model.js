bb17607845418c0a27458f16eae2d40ee60588ad
Kyle Jones <KyleJonesWinsted@gmail.com>
2020-01-07 17:04:49 -0600

    created gun and attachment models

--- attachment.model.js
+++ attachment.model.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 let Attachment = new Schema({
-
+    name: {
+        type: String
+    },
+    gunId: {
+        type: ObjectId
+    },
+    rankUnlocked: {
+        type: Number
+    },
+    imgUrl: {
+        type: String
+    },
+    slot: {
+        type: String
+    }
 })
 
 module.exports = mongoose.model('Attachment', Attachment)
--- gun.model.js
+++ gun.model.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let Gun = new Schema({
-
+    name: {
+        type: String
+    },
+    imgUrl: {
+        type: String
+    },
+    rankUnlocked: {
+        type: Number
+    },
+    category: {
+        type: String
+    }
 })
 
 module.exports = mongoose.model('Gun', Gun)
